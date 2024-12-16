ea1eb27f145c6d376c7cb5ae5dd7c078ceb789dc
hek316 <yhr05008@naver.com>
2024-12-16 15:48:43 +0900

    chore: disable existing Spring Security configuration

--- src/main/java/com/flab/mars/config/WebSecurityConfig.java
+++ src/main/java/com/flab/mars/config/WebSecurityConfig.java
@@ -2,30 +2,11 @@ package com.flab.mars.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable); //csrf 해제
-        http.headers(AbstractHttpConfigurer::disable);
-        http.httpBasic(AbstractHttpConfigurer::disable);  //http basic 비활성화
-        http.formLogin(AbstractHttpConfigurer::disable);  //기본 폼로그인 비활성화
-
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(new AntPathRequestMatcher("/api/members/*")).permitAll()
-                .anyRequest().authenticated()
-        );
-
-        return http.build();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
