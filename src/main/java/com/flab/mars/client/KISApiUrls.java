46ef2e00e47dbe492297fec59331576d9307ef8b
hek316 <yhr05008@naver.com>
2024-12-16 21:22:00 +0900

    feat: implement logic to fetch stock quotations from external API and handle auth errors
    - Developed logic to retrieve stock price quotations using the `/uapi/domestic-stock/v1/quotations/inquire-price` endpoint.
    - Configured necessary headers and query parameters for API integration.
    - Added error handling for failed or invalid API responses, including authentication errors during login.

--- src/main/java/com/flab/mars/client/KISApiUrls.java
+++ src/main/java/com/flab/mars/client/KISApiUrls.java
@@ -12,6 +12,11 @@ public class KISApiUrls {
      */
     public static final String GET_TOKEN = BASE_URL + "/oauth2/tokenP";
 
+    /**
+     * 주식현재가 시세[v1_국내주식-008]
+     */
+    public static final String INQUIRE_PRICE = BASE_URL + "/uapi/domestic-stock/v1/quotations/inquire-price";
+
     private KISApiUrls() {
         throw new AssertionError("Cannot instantiate utility class");
     }
