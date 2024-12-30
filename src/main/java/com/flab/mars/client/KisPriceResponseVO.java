4447d6b752b1e7867c5e90dbac52f83f21dae20f
hek316 <yhr05008@naver.com>
2024-12-30 18:55:20 +0900

    chore: 불필요한 응답 필드 삭제 (도메인에  필요한 데이터만 우선 취급)

--- src/main/java/com/flab/mars/client/KisPriceResponseVO.java
+++ src/main/java/com/flab/mars/client/KisPriceResponseVO.java
@@ -48,59 +48,6 @@ public class KisPriceResponseVO extends BaseResponseVO {
         @JsonProperty("stck_lwpr")
         private String stckLwpr; // 주식 최저가
 
-        @JsonProperty("stck_mxpr")
-        private String stckMxpr; // 주식 상한가
-
-        @JsonProperty("stck_llam")
-        private String stckLlam; // 주식 하한가
-
-        @JsonProperty("stck_sdpr")
-        private String stckSdpr; // 주식 기준가
-
-        @JsonProperty("wghn_avrg_stck_prc")
-        private String wghnAvrgStckPrc; // 가중 평균 주식 가격
-
-        @JsonProperty("hts_frgn_ehrt")
-        private String htsFrgnEhrt; // HTS 외국인 소진율
-
-        @JsonProperty("frgn_ntby_qty")
-        private String frgnNtbyQty; // 외국인 순매수 수량
-
-        @JsonProperty("pgtr_ntby_qty")
-        private String pgtrNtbyQty; // 프로그램매매 순매수 수량
-
-        @JsonProperty("stck_dryy_hgpr")
-        private String stckDryyHgpr; // 주식 연중 최고가
-
-        @JsonProperty("stck_dryy_lwpr")
-        private String stckDryyLwpr; // 주식 연중 최저가
-
-        @JsonProperty("w52_hgpr")
-        private String w52Hgpr; // 52주일 최고가
-
-        @JsonProperty("w52_lwpr")
-        private String w52Lwpr; // 52주일 최저가
-
-        @JsonProperty("whol_loan_rmnd_rate")
-        private String wholLoanRmndRate; // 전체 융자 잔고 비율
-
-        @JsonProperty("ssts_yn")
-        private String sstsYn; // 공매도가능여부
-
-        @JsonProperty("stck_shrn_iscd")
-        private String stckShrnIscd; // 주식 단축 종목코드
-
-        @JsonProperty("invt_caful_yn")
-        private String invtCafulYn; // 투자유의여부
-
-        @JsonProperty("mrkt_warn_cls_code")
-        private String mrktWarnClsCode; // 시장경고코드
-
-        @JsonProperty("short_over_yn")
-        private String shortOverYn; // 단기과열여부
-
-        @JsonProperty("sltr_yn")
-        private String sltrYn; // 정리매매여부
     }
 
 }
