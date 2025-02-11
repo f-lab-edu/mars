f90dc2675e84bb6099fd352598ebb8542ef65a99
hek316 <yhr05008@naver.com>
2025-02-11 18:41:09 +0900

    feat: ExecutorService를 사용하여 비동기 처리 추가
    - 주식 가격 업데이트르르 비동기적으로 처리하기 위해 ExecutorService 추가
    - 각 주식의 가격 동기화를 별도의 스레드에서 처리하여 성능 개선
    - ExecutorService 를 통한 스레드 풀 관리로 비동기 작업 효율성 향상

--- src/main/java/com/flab/mars/domain/service/StockPriceSyncService.java
+++ src/main/java/com/flab/mars/domain/service/StockPriceSyncService.java
@@ -5,6 +5,7 @@ import com.flab.mars.client.KISConfig;
 import com.flab.mars.client.dto.KisStockPriceDto;
 import com.flab.mars.db.entity.StockInfoEntity;
 import com.flab.mars.db.repository.StockInfoRepository;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
@@ -25,8 +28,22 @@ public class StockPriceSyncService {
     private final KISClient kisClient;
     private final KISConfig kisConfig;
 
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(10);
+
     // TODO - api 초당 호출 횟수 20회 제한 처리 필요
     private static final int PAGE_SIZE = 20;
+
+    private void syncStockPriceAsync(StockInfoEntity stockInfo) {
+        executor.submit(() -> {
+            try {
+                KisStockPriceDto stockPrice = kisClient.getStockPrice(kisConfig.getAccessToken(), kisConfig.getAppKey(), kisConfig.getAppSecret(), stockInfo.getStockCode());
+                stockPriceService.saveCurrentStockPrice(stockPrice, stockInfo, LocalDateTime.now());
+            } catch (Exception e) {
+                log.error("StockPriceSyncService 동기화 중 에러 발생 for stock code: {}", stockInfo.getStockCode(), e);
+            }
+        });
+    }
 
 
     public void syncStockPrices() {
@@ -37,17 +54,18 @@ public class StockPriceSyncService {
             Pageable pageable = PageRequest.of(page, PAGE_SIZE);
             Page<StockInfoEntity> stockInfoPage = stockInfoRepository.findAll(pageable);
             List<StockInfoEntity> stockInfoEntities  = stockInfoPage.getContent();
-            for (StockInfoEntity stockInfo : stockInfoEntities ) {
-                try {
-                    KisStockPriceDto stockPrice = kisClient.getStockPrice(kisConfig.getAccessToken(), kisConfig.getAppKey(), kisConfig.getAppSecret(), stockInfo.getStockCode());
-                    stockPriceService.saveCurrentStockPrice(stockPrice, stockInfo, LocalDateTime.now());
-                } catch (Exception e) {
-                    log.error("StockPriceSyncService 동기화 중 에러 발생 for stock code: {}", stockInfo.getStockCode(), e);
-                }
-            }
+            stockInfoEntities.forEach(this::syncStockPriceAsync);
 
             hasMore = stockInfoPage.hasNext();
             page++;
+        }
+   }
+
+   // 서비스 종료시 ExecutorService 를 종료하는 메서드
+   @PreDestroy
+   public void shutdownExecutorService() {
+        if(!executor.isShutdown()) {
+            executor.shutdown();
         }
    }
 }
