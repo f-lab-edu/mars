5282b34f36f40d79db03ff15425b64097253a846
hek316 <hek3167@gmail.com>
2025-02-14 18:27:28 +0900

    refactor: 기본 키 생성 전략을 GenerationType.IDENTITY로 변경
    - H2에서 AUTO_INCREMENT가 제대로 동작하지 않음

--- src/main/java/com/flab/mars/db/entity/PriceDataEntity.java
+++ src/main/java/com/flab/mars/db/entity/PriceDataEntity.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @ToString
 public class PriceDataEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
