08b7c7a6fe58dda6c9ca2940146ad56dc81e9bbd
hek316 <hek3167@gmail.com>
2025-02-14 20:05:30 +0900

    refactor: StockPriceSyncService에서 페이징 처리 제거, StockInfoFetcher로 분리
    - 서비스 로직을 비즈니스 로직에 집중시키기 위해 관심사 분리
    - StockInfoFetcher에서 지연 연산을 활용하여 메모리 과부하 방지 (stream 처리)
    - findAll() 대신 스트리밍을 적용하여 대량 데이터 조회 시 성능 개선

--- src/main/java/com/flab/mars/domain/service/StockPriceSyncService.java
+++ src/main/java/com/flab/mars/domain/service/StockPriceSyncService.java
@@ -4,18 +4,14 @@ import com.flab.mars.client.KISClient;
 import com.flab.mars.client.KISConfig;
 import com.flab.mars.client.dto.KisStockPriceDto;
 import com.flab.mars.db.entity.StockInfoEntity;
-import com.flab.mars.db.repository.StockInfoRepository;
+import com.flab.mars.domain.vo.StockInfoFetcher;
 import com.flab.mars.domain.vo.StockPriceDataVO;
 import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -24,16 +20,12 @@ import java.util.concurrent.Executors;
 @RequiredArgsConstructor
 public class StockPriceSyncService {
 
-    private final StockInfoRepository stockInfoRepository;
+    private final StockInfoFetcher stockInfoFetcher;
     private final StockPriceDataVO stockPriceDataVO;
     private final KISClient kisClient;
     private final KISConfig kisConfig;
 
-
     private final ExecutorService executor = Executors.newFixedThreadPool(10);
-
-    // TODO - api 초당 호출 횟수 20회 제한 처리 필요
-    private static final int PAGE_SIZE = 20;
 
     private void syncStockPriceAsync(StockInfoEntity stockInfo) {
         executor.submit(() -> {
@@ -46,21 +38,9 @@ public class StockPriceSyncService {
         });
     }
 
-
     public void syncStockPrices() {
-        int page = 0;
-        boolean hasMore = true;
-
-        while (hasMore) {
-            Pageable pageable = PageRequest.of(page, PAGE_SIZE);
-            Page<StockInfoEntity> stockInfoPage = stockInfoRepository.findAll(pageable);
-            List<StockInfoEntity> stockInfoEntities  = stockInfoPage.getContent();
-            stockInfoEntities.forEach(this::syncStockPriceAsync);
-
-            hasMore = stockInfoPage.hasNext();
-            page++;
-        }
-   }
+        stockInfoFetcher.fetchPagedStocksStream().forEach(this::syncStockPriceAsync);
+    }
 
     // 서비스 종료시 ExecutorService 를 종료하는 메서드
     @PreDestroy
---
+++ src/main/java/com/flab/mars/domain/vo/StockInfoFetcher.java
@@ -0,0 +1,31 @@
+package com.flab.mars.domain.vo;
+
+
+import com.flab.mars.db.entity.StockInfoEntity;
+import com.flab.mars.db.repository.StockInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Component
+@RequiredArgsConstructor
+public class StockInfoFetcher  {
+    private final StockInfoRepository stockInfoRepository;
+    private static final int PAGE_SIZE = 20;
+
+    public Stream<StockInfoEntity> fetchPagedStocksStream() {
+        return Stream.iterate(0, page -> page + 1)
+                .map(page -> {
+                    Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+                    Page<StockInfoEntity> stockPage = stockInfoRepository.findAll(pageable);
+                    return stockPage.hasContent() ? stockPage.getContent() : null;
+                })
+                .takeWhile(stocks -> stocks != null && !stocks.isEmpty())
+                .flatMap(List::stream);
+    }
+}
